f178b2b52e6b1182ddbf35b5299be31b49976ff2
Leandro Roldan <leandrom.roldan@gmail.com>
2023-02-14 23:21:02 -0300

    fix: welcome section image

--- components/welcome/index.jsx
+++ components/welcome/index.jsx
@@ -12,8 +12,8 @@ export const Welcome = ({ title, description, cv }) => {
 			<div className='flex items-center justify-center rounded-full bg-cyan-600 lean w-[230px] h-[230px] sm:w-[294px] sm:h-[294px] xl:w-[390px] xl:h-[390px] bg-gradient-to-r bg-clip-border bg-transparent from-cyan-800 via-cyan-500 to-cyan-300 animate-text'>
 				<Image
 					className='lean bg-cyan-100 rounded-full w-56 sm:w-72 xl:w-96 h-56 sm:h-72 xl:h-96 '
-					width={3103}
-					height={3103}
+					width={2500}
+					height={2500}
 					src='https://res.cloudinary.com/deooec1tp/image/upload/v1675355039/My%20web/Lean_sin_fondo_zan07l.png'
 					alt='Leandro'
 				/>
